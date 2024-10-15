bc7f11e5120ae8daeb407a83579e4bc423195f65
Isaac Barnes <isaacbarnes0084@gmail.com>
2024-10-15 22:51:49 +1100

    Datavis 2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,20 @@
 var vg_1 = "choropleth.json"
 var vg_2 = "line_animated.json"
+var vg_3 = "natural_disasters.json"
+var vg_4 = "CO2_Continents_Bar.json"
 
 vegaEmbed("#Choropleth_Map", vg_1).then(function(result) {
     
 }).catch(console.error);
 
 vegaEmbed("#Line_Animated", vg_2).then(function(result) {
+    
+}).catch(console.error);
+
+vegaEmbed("#Natural_Disasters", vg_3).then(function(result) {
+    
+}).catch(console.error);
+
+vegaEmbed("#CO2_Bar", vg_4).then(function(result) {
     
 }).catch(console.error);
